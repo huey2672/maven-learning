ae542e77813e1c9efe55002cbeaa368604f476a0
huey <huey2672@163.com>
2021-01-20 11:39:52 +0800

    executable jar

---
+++ executable-jar-sample/using-assembly-plugin/executable-jar-sample/src/main/java/com/huey/learning/maven/practice/ExecutableJarSample.java
@@ -0,0 +1,11 @@
+package com.huey.learning.maven.practice;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class ExecutableJarSample {
+
+	public static void main(String[] args) {
+		System.out.println(StringUtils.join("a", "b", "c"));
+	}
+
+}
